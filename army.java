3fd611ce1c9e0544029e9697a0dc190f4609ad3d
dhruvi1104 <47188729+dhruvi1104@users.noreply.github.com>
2019-02-22 12:59:32 -0500

    divided main method in separate class
    Calling of each function of army class from main method

--- army.java
+++ army.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.HashMap;
 
 
-public class army {
+public class army extends mainmethod{
 	
 	public static Integer totalArmy(int no_of_players, int total_country) {
 		int armies_per_player = 0;
@@ -129,36 +129,5 @@ public class army {
 		
 		return army_per_country;
 	}
-	
 
-	
-	public static void main(String args[]) {
-		
-		int n=5; //total number of players
-		String[] p= {"Krisha","Dhruvi","Pavan","Koti","Charan"}; //players name
-		int total_country = 42;
-		ArrayList<String> country_name = new ArrayList<String>();
-		for(int l=0;l<total_country;l++) {
-			country_name.add("c"+l);
-		}	
-		List<List<String>> country_per_player = new ArrayList<List<String>>();
-		
-		//total army to players
-		int armies_per_player = totalArmy(n,total_country);
-		System.out.println("Total No. of countries : "+total_country);
-		System.out.println("Per Player armies : "+armies_per_player);
-		
-		//divide countries
-		country_per_player = divideCountry(p,country_name);
-		System.out.println("\nList of country per player");
-		System.out.println(country_per_player);
-		
-		
-		//divide armies to countries
-		HashMap<String,Integer> army_per_country = new HashMap<String,Integer>();
-		army_per_country = armyPerCountry(p,armies_per_player,country_per_player);
-		System.out.println("No. of armies per country");
-		System.out.println(army_per_country);
-		
-	}
 }
