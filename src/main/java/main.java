6b643fae59a65f0a9cd73085068d0ce38fca2a4e
dhruvi1104 <dhruvigadhiya11@gmail.com>
2019-03-22 16:48:28 -0400

    views updates

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -61,10 +61,26 @@ public class main {
         for(int i=0;i<total_players;i++) {
         	
         	//Players world domination view
-        	System.out.println("Map(%) : ");
-        	System.out.println("Continents : ");
-        	System.out.println("Total armies : ");
+        	for(int j=0;j<total_players;j++) {
+        		int total_country_num = country_list.size();
+        		int player_country_num = (country_per_player.get(j)).size();
+        		float map_per_player = (float)(100*player_country_num)/total_country_num;
+        		System.out.println("Map(%) : "+map_per_player+" %");
+        		
+        		ArrayList<String> continent_list_per_player;
+        		for(int k=0;k<country_per_player.get(j).size();k++){
+        			
+        		}
+            	//System.out.println("Continents : "+continent_list_per_player);
+            	
+            	int total_army_per_player=0;
+            	for(int k=0;k<country_per_player.get(j).size();k++){
+            		total_army_per_player+=army_per_country.get(country_per_player.get(j).get(k));
+            	}
+            	System.out.println("Total armies : "+total_army_per_player);
+        	}
         	
+        	System.exit(0);
         	//phase view
         	System.out.println("Reinforcement Phase");
         	System.out.println("Player : "+player_names[i]);
